b2e485cba15bc43f90730c121f24e5952b01b7a3
Robert Schwartz <36746526+rschwartz1223@users.noreply.github.com>
2019-01-25 00:00:27 -0800

    Changed method of sampling

--- Source.cpp
+++ Source.cpp
@@ -1,45 +1,52 @@
-/* Calculates the integral from x=0 to x=1 of f(x) = cos^2(x) using Monte
-   Carlo simulation, which uses random sampling within a range to find
-   average values. To change the range of integration, modify LINE 21. To
-   change the integrand, modify LINE 27 */
+/* 
+ *  Calculates the integral from x=0 to x=1 of f(x) = cos^2(x) using Monte Carlo 
+ *  simulation with coordinates generated using the Golden Ratio and modf() as a pseudo-rng.
+ */
 
-#include <random>
 #include <iostream>
-#include <cmath>
+#include <cmath> // pow(), cos()
+#include <math.h> // modf()
 
-using namespace std;
 
-/* calculate the integral of function defined in this body
-   @param n amount of samples 
-   @return sum/n average value of all samples */
-double integrate(int n) {
+// global constant
+const double golden_ratio = 1.61803398875;
 
-	// rng
-	default_random_engine rng;
+/* 
+ *  calculate the integral of function using Golden Ratio 
+ *
+ *  @param n amount of samples
+ *  @return sum/n average value of all samples
+ */
+double golden_ratio_integrate(int n)
+{
+  // starting position
+  double x = 0.5;
+  // intpart for modf()
+  double intpart = 1.0;
 
-	// range of values to be generated
-	uniform_real_distribution<double> distribution(0.0, 1.0);
-
-	double sum{ 0 };
+	double sum = 0.0;
 	
-	// find sum of the values of the samples
+	// sum the sample values
 	for (int i = 0; i < n; ++i)
-		sum += pow(cos(distribution(rng)), 2);
+		sum += pow(cos(modf((x + golden_ratio) * i, &intpart)), 2);
 
 	// return average value
-	return sum / n;
+	return (sum / n);
 }
 
-int main() {
-
+int main()
+{
+  // samples variable
 	int n;
-	cout << "Enter the amount of samples you wish to calculate (3 = 10^3): ";
-	cin >> n;
+	std::cout << "Enter the amount of samples you wish to calculate (3 = 10^3): ";
+	std::cin >> n;
 
-	// display output done from 10^i samples, where i = n
-	for (int i = 0; i <= n; ++i)
-		cout << pow(10, i) << " samples: " << integrate(pow(10, i)) << endl;
+  // display output done from 10^i samples, where i = n
+  std::cout << "\nSampling using the Golden Ratio" << std::endl;
+	for(int i = 0; i <= n; ++i)
+    std::cout << pow(10, i) << " samples: " << golden_ratio_integrate(pow(10, i)) << std::endl;
 
 	system("PAUSE");
 	return 0;
+
 }
